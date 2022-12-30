60f31d9d500630d26ca29d91f3ab49c0067383ad
Lionello Lunesu <lio+git@lunesu.com>
2022-12-30 10:50:24 -0800

    Add chrome support

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,40 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as open from "open";
+
+export const CHROME_REGEX = /https:\/\/g\.co\/sc\b/g;
+
+interface CustomTerminalLink extends vscode.TerminalLink {
+  url: string;
+  app: open.App;
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.window.registerTerminalLinkProvider({
+    provideTerminalLinks: (
+      context: vscode.TerminalLinkContext,
+      token: vscode.CancellationToken
+    ) => {
+      const matches = [...context.line.matchAll(CHROME_REGEX)];
+      return matches.map((match) => {
+        return {
+          startIndex: match.index,
+          length: match[0].length,
+          tooltip: "Open in chrome",
+          url: match[0],
+          app: { name: open.apps.chrome },
+        } as CustomTerminalLink;
+      });
+    },
+    handleTerminalLink: async (link: CustomTerminalLink) => {
+      await open(link.url, { app: link.app });
+    },
+  });
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "which-browser" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('which-browser.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from which-browser!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
